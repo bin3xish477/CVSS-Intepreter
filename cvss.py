9c2a395be86f01870d48034a973c24bdcf267bc1
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-04-04 12:03:28 -0700

    updating repo

--- cvss.py
+++ cvss.py
@@ -7,7 +7,7 @@ __email__ = 'rodriguez10011999@gmail.com'
 """
 Sources used for scoring descriptions:
   - CompTIA CySa+ Study Guide by Mike Chapple and David Seidl
-  - https://www.first.org/cvss/specification-document
+  - https://www.first.org/cvss/v3.0/specification-document
 
   Example CVSS v2 : CVSS2#AV:N/AC:L/Au:N/C:C/I:C/A:C
   Example CVSS v3 : CVSS:3.1/AV:N/AC:L/PR:H/UI:N/S:U/C:L/I:L/A:N
@@ -27,10 +27,15 @@ access_vector = {
   'P': 'The attack requires the attacker to physically touch or manipulate the vulnerable component.'
 }
 
-access_complexitiy = {
+access_complexitiyv2 = {
   'L': 'Low (L): Exploiting the vulnerability does not require specilized conditions.',
   'M': 'Medium (M): Exploiting the vulnerability requires "somewhat specilized" conditions.',
   'H': 'High (H): Exploiting the vulnerability requires "specialized" conditions that would be difficult to find.'
+}
+
+access_complexitiyv3 = {
+  'H': 'High (H): A successful attack depends on conditions beyond the attacker\'s control. That is, a successful attack cannot be accomplished at will, but requires the attacker to invest in some measurable amount of effort in preparation or execution against the vulnerable component before a successful attack can be expected.',
+  'L': 'Low (L): Specialized access conditions or extenuating circumstances do not exist. An attacker can expect repeatable success against the vulnerable component.'
 }
 
 privileges_required = {
@@ -48,7 +53,6 @@ scope = {
   'U': 'Unchanged (U): An exploited vulnerability can only affect resources managed by the same security authority. In this case, the vulnerable component and the impacted component are either the same, or both are managed by the same security authority.',
   'C': 'Changed (C): An exploited vulnerability can affect resources beyond the security scope managed by the security authority of the vulnerable component. In this case, the vulnerable component and the impacted component are different and managed by different security authorities.'
 }
-# ------------------------------------------------------------- #
 
 authentication = {
   'N': 'None (N): Attackers do not need to authenticate to exploit the vulnerability.',
@@ -56,6 +60,7 @@ authentication = {
   'M': 'Multiple (M): Attackers would need to authenticate two or more times exploit the vulnerability.'
 }
 
+#   Impact Metrics for v2
 confidentialityv2 = {
   'N': 'None (N): There is no confidentiality impact.',
   'P': 'Partial (P): Access to some information is possible, but the attacker does not have control over what information is compromised.',
@@ -74,17 +79,23 @@ availabilityv2 = {
   'C': 'Complete (C): The system is completely shut down.'
 }
 
-#   Impact Metric for V3
+#   Impact Metrics for V3
 confidentialityv3 = {
-
+  'H': 'High (H): There is total loss of confidentiality, resulting in all resources within the impacted component being divulged to the attacker',
+  'L': 'Low (L): There is some loss of confidentiality. Access to some restricted information is obtained, but the attacker does not have control over what information is obtained, or the amount or kind of loss is constrained',
+  'N': 'None (N): There is no loss of confidentiality within the impacted component.'
 }
 
 integrityv3 = {
-
+  'H': 'High (H): There is a total loss of integrity, or a complete loss of protection.',
+  'L': 'Low (L): Modification of data is possible, but the attacker does not have control over the consequence of a modification, or the amount of modification is constrained.',
+  'N': 'None (N): There is no loss of integrity within the impacted component.'
 }
 
 availabilityv3 = {
-
+  'H': 'High (H): There is total loss of availability, resulting in the attacker being able to fully deny access to resources in the impacted component; this loss is either sustained (while the attacker continues to deliver the attack) or persistent (the condition persists even after the attack has completed)',
+  'L': 'Low (L): There is reduced performance or interruptions in resource availability',
+  'N': 'None (N): There is no impact to availability within the impacted component.'
 }
 
 class Cvss2:
@@ -126,19 +137,19 @@ class Cvss2:
       return access_vector[value]
 
     elif identifier == 'ac':
-      return access_complexitiy[value]
+      return access_complexitiyv2[value]
 
     elif identifier == 'au':
        return authentication[value]
 
     elif identifier == 'c':
-      return confidentiality[value]
+      return confidentialityv2[value]
 
     elif identifier == 'i':
-      return integrity[value]
+      return integrityv2[value]
 
     else:
-      return availability[value]
+      return availabilityv2[value]
 
 class Cvss3:
   """CVSS version 3 class definition."""
@@ -178,10 +189,7 @@ class Cvss3:
       return access_vector[value]
 
     elif identifier == 'ac':
-      return access_complexitiy[value]
-
-    elif identifier == 'au':
-       return authentication[value]
+      return access_complexitiyv3[value]
 
     elif identifier == 'pr':
       return privileges_required[value]
@@ -193,13 +201,13 @@ class Cvss3:
       return scope[value]
 
     elif identifier == 'c':
-      return confidentiality[value]
+      return confidentialityv3[value]
 
     elif identifier == 'i':
-      return integrity[value]
+      return integrityv3[value]
 
     else:
-      return availability[value]
+      return availabilityv3[value]
 
 def main():
   if len(argv) == 1 or len(argv) > 2:
@@ -214,13 +222,15 @@ def main():
 
   score_dict = instance.parse_score()
   results = instance.determine_metrics(score_dict)
-  
+
+  print('\n' + '-'*150)
   for key, val in results.items():
     print('\033[31m\033[1m' + key + '\033[0m -> ', end=' ')
     val_list = val.split()
     print('\x1b[1;33m' + val_list[0] + '\033[0m', end=' ')
     print('\033[33m' + val_list[1] + '\033[0m', end=' ')
     print(' '.join(val_list[2:]))
+  print('-'*150, '\n')
 
 if __name__ == '__main__':
   try:
